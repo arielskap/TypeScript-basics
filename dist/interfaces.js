298db3a6bd466d9ab7d7bc59ec4fd0771bbeb26a
Ariel Villarreal <arielskap@gmail.com>
2020-09-02 10:49:21 -0300

    interfaces

---
+++ dist/interfaces.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var PhotoOrientation;
+(function (PhotoOrientation) {
+    PhotoOrientation[PhotoOrientation["Landscape"] = 0] = "Landscape";
+    PhotoOrientation[PhotoOrientation["Portrait"] = 1] = "Portrait";
+    PhotoOrientation[PhotoOrientation["Square"] = 2] = "Square";
+    PhotoOrientation[PhotoOrientation["Panorama"] = 3] = "Panorama";
+})(PhotoOrientation || (PhotoOrientation = {}));
+function showPicture(picture) {
+    console.log("[title: " + picture.title + ",\n    date: " + picture.date + ",\n  orientation: " + picture.orientation + "]");
+}
+var myPic = {
+    title: 'Test Title',
+    date: '2020-03',
+    orientation: PhotoOrientation.Landscape
+};
+showPicture(myPic);
+showPicture({
+    title: 'Test Title',
+    date: '2020-03',
+    orientation: PhotoOrientation.Portrait
+});
